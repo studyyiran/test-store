5b82e64fade204a3c169f40970401ed7b1620b54
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-26 11:58:13 +0800

    版本1

--- src/pages/testBindJs/myBind.js
+++ src/pages/testBindJs/myBind.js
@@ -1,9 +1,21 @@
 Function.prototype.myBind = function(that, ...other) {
   // call apply bind
-    console.log(this)
+    console.log(this.call)
+    const me = this
   const newFunc = function(...originParams) {
-    return this.call(that, originParams.concat(other));
+        // 我以为 这个this指的是调用的函数.
+      // 这个bug是因为,newFunc无法决定他的this指向,因此我用闭包拿一下
+      // apply 是数组 call是参数列表
+    return me.apply(that, originParams.concat(other));
   };
   return newFunc;
 };
+
+Function.prototype.lucasBind = function (context) {
+    var me = this;
+    var argsArray = Array.prototype.slice.call(arguments);
+    return function () {
+        return me.apply(context, argsArray.slice(1))
+    }
+}
 
--- src/pages/testBindJs/myBind.test.js
+++ src/pages/testBindJs/myBind.test.js
@@ -1,17 +1,27 @@
 import something from './myBind'
 
-function hehe(obj) {
-  const a = function() {
-    console.log(this && this.name);
-    return this.name;
-  };
-  const b = a.myBind(obj);
-  return b
-}
+const a = function(...other) {
+  console.log(this && this.name);
+  return [this.name].concat(other);
+};
 
-it("", () => {
+it("me", () => {
   const obj = {
     name: "haha"
   };
-  expect(hehe(obj)()).toBe(obj.name);
+  const second = 2
+  const result = a.myBind(obj)(second)
+  expect(result[0]).toBe(obj.name);
+  expect(result[1]).toBe(second);
+});
+
+
+it("lucas", () => {
+  const obj = {
+    name: "haha"
+  };
+  const second = 2
+  const result = a.lucasBind(obj)(second)
+  expect(result[0]).toBe(obj.name);
+  expect(result[1]).toBe(second);
 });
