e4189b28249f6992587a664fd96213a63d3a7208
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-12 14:32:31 +0800

    完成了抽象逻辑的制作,支持打印

--- src/pages/xOr/index2.tsx
+++ src/pages/xOr/index2.tsx
@@ -57,6 +57,19 @@ const gateXOR: ICommonInput = (inputA, inputB) => {
   }
 };
 
+const gateXOR2: ICommonInput = (inputA, inputB) => {
+  if (inputA !== undefined && inputB !== undefined) {
+    /*
+
+     */
+    const a = gateAnd(inputA, gateNot(inputB));
+    const b = gateAnd(inputB, gateNot(inputA));
+    return gateOr(a, b);
+  } else {
+    return FALSE;
+  }
+};
+
 export default function() {
   const [valueA, setValueA] = useState(undefined);
   const [valueB, setValueB] = useState(undefined);
@@ -68,12 +81,56 @@ export default function() {
       func(0);
     }
   }
+
+  function renderItem(valueA: number, valueB: number, valueResult: number) {
+    return (
+      <tr>
+        <th>{valueA}</th>
+        <th>{valueB}</th>
+        <th>{valueResult}</th>
+      </tr>
+    );
+  }
+  function renderList(valueArr: number[], gateFunc: any, gateName: string) {
+    const resultDomArr: any[] = [];
+    valueArr.forEach((itemA, indexA) => {
+      valueArr.forEach((itemB, indexB) => {
+        resultDomArr.push(renderItem(itemA, itemB, gateFunc(itemA, itemB)));
+      });
+    });
+    return (
+      <div>
+        <h3>{gateName}</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>inputA</th>
+              <th>inputB</th>
+              <th>output</th>
+            </tr>
+          </thead>
+          <tbody>{resultDomArr}</tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
     <div>
       <input value={valueA} onChange={handler.bind({}, setValueA)} />
       <input value={valueB} onChange={handler.bind({}, setValueB)} />
-      <div>result gateOr: {gateOr(valueA, valueB)}</div>
-      <div>result gateXOR: {gateXOR(valueA, valueB)}</div>
+      <div>
+        <h2>这个用于动态显示计算</h2>
+        <div>result gateOr: {gateOr(valueA, valueB)}</div>
+      </div>
+
+      {renderList([0, 1], gateOr, "gateOr")}
+      {renderList([0, 1], gateAnd, "gateAnd")}
+      {renderList([0, 1], gateNAND, "gateNAND")}
+
+      {renderList([0, 1], gateXOR, "gateXOR")}
+      {/*<div>result gateXOR2: {gateXOR2(valueA, valueB)}</div>*/}
+      {renderList([0, 1], gateXOR2, "gateXOR2")}
     </div>
   );
 }
@@ -82,4 +139,6 @@ export default function() {
 实现了一个抽象的异或门.xOr
 对抽象电路有了一点点的了解.
 但是我没办法将昨天的内容使用20分钟复习法进行复习.因为那个非常业务逻辑.没有意义.
+
+次日复习的时候,我进行了进一步的拓展,将他们做成了可以自己打印的.很爽.感觉接下来,我距离bool运算加法,并不远了.
  */
