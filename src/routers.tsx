4c44dc6cb113b6a8239a9c61223d9ecb9cb5557d
yiranzi <570932296@qq.com>
2019-12-21 11:23:41 +0800

    增加默认

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,29 +1,36 @@
 import React from "react";
-import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  Router,
+  Switch,
+  Route,
+  BrowserRouter,
+  Redirect
+} from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { routerConfig } from "./routerConfig";
 import { TestUseMemo, TestUseMemoContextProvider } from "./pages/useMemo";
 import { ShareComponent } from "./share/share";
 const myHistory = createBrowserHistory();
-
+const current = '/gate-box-2'
 
 export default function Routers() {
   return (
     <BrowserRouter>
-        <ShareComponent>
-      <Switch>
-        {routerConfig.map(item => {
-          const { path, Component } = item;
-          return (
-            <Route
-              key={path}
-              path={path}
-              component={layoutHoc(Component)}
-            ></Route>
-          );
-        })}
-      </Switch>
-            </ShareComponent>
+      <ShareComponent>
+        <Switch>
+          <Redirect to={current} />
+          {routerConfig.map(item => {
+            const { path, Component } = item;
+            return (
+              <Route
+                key={path}
+                path={path}
+                component={layoutHoc(Component)}
+              ></Route>
+            );
+          })}
+        </Switch>
+      </ShareComponent>
     </BrowserRouter>
   );
 }
