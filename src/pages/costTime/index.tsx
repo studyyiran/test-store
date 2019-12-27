55b746e6655a03362cb8ecd40e0ea2424d8ee5f5
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-27 18:49:13 +0800

    cost time

--- src/pages/costTime/index.tsx
+++ src/pages/costTime/index.tsx
@@ -1,21 +1,7 @@
 import React, { useState } from "react";
+import "./index.scss";
 
-export default function CostTimeContainer() {
-  const [count, setCount] = useState(0);
-  function handler() {
-      console.log('handler')
-    setCount(count => count + 1);
-  }
-  return (
-    <div>
-      <div onClick={handler}>add</div>
-      <input onChange={handler} />
-      <CostTime count={count} />
-    </div>
-  );
-}
-
-function CostTime(props: any) {
+function CostTimeCostTime(props: any) {
   console.log(props.count);
   function renderList() {
     const arr = [];
@@ -24,5 +10,59 @@ function CostTime(props: any) {
     }
     return arr;
   }
-  return <ul>{renderList()}</ul>;
+  return (
+    <ul>
+      <h1>{props.finalValue}</h1>
+      {renderList()}
+    </ul>
+  );
+}
+
+// const CostTime = React.memo(CostTimeCostTime, () => false)
+const CostTime = React.memo(CostTimeCostTime, (prevProps, nextProps) => {
+  if (prevProps.finalValue !== nextProps.finalValue) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
+export default function() {
+  return (
+    <div className="cost-time-container">
+      <div>
+        <CostTimeContainer
+          render={(props: any) => {
+            return <CostTime {...props} finalValue={'我很快' + props.finalValue} />;
+          }}
+        />
+      </div>
+      <div>
+        <CostTimeContainer
+          render={(props: any) => {
+            return <CostTimeCostTime {...props} finalValue={'我很慢' + props.finalValue} />;
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function CostTimeContainer(props: any) {
+  const [value, setValue] = useState(0);
+  const [finalValue, setFinalValue] = useState(0);
+  function onInputHandler(e: any) {
+    setValue(e.target.value);
+  }
+  function onClickHandler(e: any) {
+    setFinalValue(value);
+    // setValue(e.target.value);
+  }
+  return (
+    <div>
+      <input onInput={onInputHandler} />
+      <button onClick={onClickHandler}>提交</button>
+      {props.render({ value, finalValue })}
+    </div>
+  );
 }
