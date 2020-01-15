f8581fd43a460c26b3a4a2c61c16a6226918ad60
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-15 22:31:50 +0800

    test

--- src/pages/cssPlayGround/margin/index.tsx
+++ src/pages/cssPlayGround/margin/index.tsx
@@ -6,8 +6,9 @@ export function Margin1() {
         console.log(e.target.clientWidth)
     }
     return <div className="margin">
-        <div onClick={getWidth}>
-            margin水平方向
+        <div className="test-margin-container" onClick={getWidth}>
+            {/*margin水平方向*/}
+            <img src={require('../../../res/test.png')}/>
             <p className="test-margin">当为标准block 并且没有设定 width的时候 通过设置margin-left 会影响容器的宽度</p>
         </div>
         <ul>
