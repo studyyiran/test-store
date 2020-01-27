26e421f62d935f72b93681a30e5b71e80bfc786b
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-27 09:19:31 +0800

    test

--- node/replace/index.js
+++ node/replace/index.js
@@ -7,19 +7,19 @@ http
       encoding: "utf-8"
     });
     const htmlDes = "adfadfadfadf";
-    template = template.replace(
-      /\<meta name=\"description\" content=\"\"\ \/>/,
-      '<meta name="description" content="' + htmlDes + '">'
-    );
-    const json = {
+    const jsonInfo = {
       a: 'a',
       b: 'b',
     }
-    template = template.replace(
-      /\<script type=\"application\" \/>/,
-      `<script type="application/ld+json">${JSON.stringify(json)}</script>`
-    );
-    console.log(template);
+      template = template.replace(
+          /\<meta name=\"description\" content=\"\"\/>/,
+          '<meta name="description" content="' + htmlDes + '">'
+      );
+      template = template.replace(
+          /\<script type=\"application\">\<\/script\>/,
+          `<script type="application/ld+json">${jsonInfo}</script>`
+      );
+
     response.write(template);
     response.end();
   })
