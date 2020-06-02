35e822a4d239e5e130d400804661a029783d18a8
yiranzi <570932296@qq.com>
2020-06-02 16:20:49 +0800

    测试webpack 打包 node完成

--- testweb/src/server.js
+++ testweb/src/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import path from 'path'
 
 export const app = express();
+// app.use('/static', express.static(path.resolve(__dirname, './build')))
+console.log(__dirname)
+app.use('/static', express.static(__dirname));
+// app.use('/static', express.static('build'));
+app.get('/static/*', (req, res) => {
+  res.send(path.resolve(__dirname));
+})
 app.get('/a', (req, res) => {
     res.write('a')
     res.end();
 })
-const port = '8080'
-app.listen('8080', () => {
+const port = '8081'
+app.listen(port, () => {
     console.log(__dirname + port)
 })
 
