30e8932f0da50b06280ee23b8c6a403e83e664cc
yiranzi <570932296@qq.com>
2020-06-03 14:11:43 +0800

    测试webpack 打包 node完成

--- testweb/src/server.js
+++ testweb/src/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import path from 'path'
-
-export const app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+const app = express();
 // app.use('/static', express.static(path.resolve(__dirname, './build')))
 console.log(__dirname)
 app.use('/static', express.static(__dirname));
@@ -10,6 +10,7 @@ app.get('/static/*', (req, res) => {
   res.send(path.resolve(__dirname));
 })
 app.get('/a', (req, res) => {
+    console.log(req.headers['user-agent'])
     res.write('a')
     res.end();
 })
@@ -18,4 +19,13 @@ app.listen(port, () => {
     console.log(__dirname + port)
 })
 
+function test(a, b) {
+  console.log(this)
+  console.log(a)
+  console.log(b)
+}
+const content = fs.readFileSync('src/index.js')
+test.call({hehe: 'hehe'}, 1 , 2)
+process.stdout.write(content);
+module.exports = app
 
